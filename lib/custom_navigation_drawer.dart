c04785edc0ee0d30eaabf5c5b583e23e2043cd0f
MaryamElmtibaa <maryamelmtibaa@gmail.com>
2023-10-12 00:10:56 +0100

    final commit

---
+++ lib/custom_navigation_drawer.dart
@@ -0,0 +1,3 @@
+export 'commons/collapsing_list_tile_widget.dart';
+export 'widgets/navigation_model.dart';
+export 'utils/theme.dart';
---
+++ lib/pages/team_page.dart
@@ -0,0 +1,336 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/text_style.dart';
+import '../widgets/drawer.widget.dart';
+
+String mode = "Jour";
+FirebaseDatabase fire = FirebaseDatabase.instance;
+DatabaseReference ref = fire.ref();
+
+class TeamPage extends StatefulWidget {
+  @override
+  State<TeamPage> createState() => _TeamPage();
+}
+
+class _TeamPage extends State<TeamPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //drawer: CollapsingNavigationDrawer(),
+        drawer: MyDrawer(),
+        body: SingleChildScrollView(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(19.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Text(
+                        "Compétances",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            color: Colors.indigo,
+                            shadows: [
+                              Shadow(
+                                  offset: Offset(5, 5),
+                                  color: Colors.black.withOpacity(0.5),
+                                  blurRadius: 10)
+                            ]),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(height: 30),
+                      Text(
+                        "Language",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(5, 5),
+                              color: Colors.black.withOpacity(0.5),
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                      SizedBox(width:5),_buildFrameworkItem("JAVA"), SizedBox(width:10),    _buildFrameworkItem("C#"),
+                            SizedBox(width:10),   _buildFrameworkItem("PHP"),
+                            SizedBox(width:10),   _buildFrameworkItem("IA"),
+                            SizedBox(width:10),  _buildFrameworkItem("C"),
+                            SizedBox(width:10),  _buildFrameworkItem("Mobile"),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Framework",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(5, 5),
+                              color: Colors.black.withOpacity(0.5),
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                        /*decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),*/
+                        //child: Padding(
+                        // padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                          Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                              //SizedBox(width:10),
+                                _buildFrameworkItem("Symfony"),
+                              SizedBox(width:5),   _buildFrameworkItem("Laravel"),
+
+
+                              SizedBox(width:5),
+                            _buildFrameworkItem("Asp.net"),
+                                SizedBox(width:5),
+                              _buildFrameworkItem("Sélénium"),]),
+
+                          ]),
+                        //),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Conception",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.blue,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(5, 5),
+                              color: Colors.black.withOpacity(0.5),
+                              blurRadius: 10,
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+
+                              SizedBox(width:5) ,_buildFrameworkItem("Merise"),
+                              SizedBox(width:10),_buildFrameworkItem("Uml"),
+                            ],
+
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(9.0),
+                            child: Text(
+                              "Langues",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.blue,
+                                  shadows: [
+                                    Shadow(
+                                        offset: Offset(5, 5),
+                                        color: Colors.black.withOpacity(0.5),
+                                        blurRadius: 10)
+                                  ]),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20,),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 10, right: 50, top: 0, bottom: 10),
+                        child: _buildFrameworkItem("Arabe : Maternelle"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 10, right: 50, top: 0, bottom: 10),
+                        child: _buildFrameworkItem("Francais : Moyen"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            left: 10, right: 50, top: 0, bottom: 10),
+                        child: _buildFrameworkItem("Anglais : Moyen"),
+                      ),
+                    ],
+
+                  )
+                ]))));
+
+    //  _itemWidget("Framework : "," \nSymfony, Laravel, Asp.net, Sélénium"),
+    //  _itemWidget("Conception : ","\nMerise, Uml"),
+    //   _itemWidget("SGBD : "," \nSql Server, Wampserver, Laragon\n\n"),
+
+    /////mode nuit et mode jour
+    /*  Text("Mode",style:TextStyle(fontSize: 22),),
+                     ListTile(
+                       title: Text("Jour"),
+                       leading: Radio<String>(
+                         value:"Jour",
+                         groupValue: "mode",
+                         onChanged: (value){
+                           setState((){
+                             mode=value!;
+                           });
+                         },
+                       ),
+                     ),
+                      ListTile(
+                        title: Text("Nuit"),
+                        leading: Radio<String>(
+                          value:"Nuit",
+                          groupValue: "mode",
+                          onChanged: (value){
+                            setState((){
+                              mode=value!;
+                            });
+                          },
+                        ),
+                      ),
+                    Container(
+                       padding: EdgeInsets.all(10),
+                       child:ElevatedButton(
+                         style:ElevatedButton.styleFrom(
+                           minimumSize: const Size.fromHeight(50)
+                         ),
+                           onPressed: (){_onSaveMode();}, child: Text("Enregistrer"))
+                     ),*/
+  }
+
+  Widget _itemWidget(title, value) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: subHeaderTextStyle,
+            ),
+            Center(
+                child: Text(
+              value,
+              style: bodyTextStyle,
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+
+  Widget _itemWidgett(title, value) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: subHeaderTextStyle,
+            ),
+            Center(
+                child: Text(
+                  value,
+                  style: bodyStyle,
+                )),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+
+  Widget _itemWidgete(title, value) {
+
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            children: [
+              Container(
+                  child: Text(
+                title,
+                style: subHeaderTextStyle,
+              )),
+              Text(
+                value,
+                style: bodyTextStyle,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildFrameworkItem(String s) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.grey,width: 1),
+        borderRadius: BorderRadius.circular(8.0),),
+
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          s,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+      ),
+    );
+  }
+/*void  _onSaveMode() async {
+    await  ref.set({"mode":mode});
+    print("mode applique $mode");
+    Navigator.pop(context);
+
+
+  }*/
+}
